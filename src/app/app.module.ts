bd25f00a092bef73c1843449c721ab7790180c4c
rreganjr <rreganjr@acm.org>
2019-04-18 22:45:09 -0400

    wrapping with menu and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule,
-  MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatRadioModule} from '@angular/material';
-
+import {MatAutocompleteModule, MatMenuModule, MatInputModule, MatOptionModule, MatSelectModule,
+  MatIconModule, MatListModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule,
+   MatRadioModule, MatToolbarModule, MatButtonModule, MatSidenavModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatAutocompleteModule,
+    MatMenuModule,
     MatInputModule,
     MatOptionModule,
     MatSelectModule,
     MatIconModule,
+    MatListModule,
     MatCheckboxModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-nav/top-nav.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-top-nav',
+  templateUrl: './top-nav.component.html',
+  styleUrls: ['./top-nav.component.scss']
+})
+export class TopNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
